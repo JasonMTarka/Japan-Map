881b17b45a7ab8ace77cc200f297e23831a6af1d
Jason <71971736+JasonMTarka@users.noreply.github.com>
2023-02-23 17:25:36 +0900

    First commit
    - Initialized using Create React App
    - Converted to Typescript
    - Removed unnecessary code

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./css/App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Japan Map</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
